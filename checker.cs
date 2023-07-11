4785b1dc40e842a18ff722f3ac818110b3038d8b
ces039 <ces039@bucknell.edu>
2023-07-11 12:29:14 -0400

    progress but problems with loading screen

--- checker.cs
+++ checker.cs
@@ -6,52 +6,82 @@ using System.Net;
 using HtmlAgilityPack;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace RoyalCaribbeanChecker
 {
     public class checker
     {
         [FunctionName("checker")]
-        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             string url = "https://www.royalcaribbean.com/booking/landing?groupId=AL03PCN-3464559352&sailDate=2024-02-09&shipCode=AL&packageCode=AL3BH068&destinationCode=BAHAM&selectedCurrencyCode=USD&country=USA&loyaltyNumber=380604273&CAS=true";
-            string spanId = "pricing-promotions__price-value";
+        
 
-        if (!string.IsNullOrEmpty(url) && !string.IsNullOrEmpty(spanId))
+        // Class name of the span element to scrape
+        //string className = "pricing-promotions__price-value";
+        string spanId = "landingPage_totalPrice_text";
+
+        // Create an HttpClient instance to send HTTP requests
+        using (HttpClient client = new HttpClient())
         {
-            try
+            // Send a GET request to the website
+            HttpResponseMessage response = await client.GetAsync(url).Result;
+
+            // Check if the request was successful
+            if (response.IsSuccessStatusCode)
             {
-                WebClient webClient = new WebClient();
-                string html = webClient.DownloadString(url);
+                await Task.Delay(5000);
+                // Read the response content as a string
+                string htmlContent = await response.Content.ReadAsStringAsync();
 
-                HtmlDocument doc = new HtmlDocument();
-                doc.LoadHtml(html);
+                // Load the HTML content into HtmlAgilityPack's HtmlDocument
+                HtmlDocument htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(htmlContent);
 
-                HtmlNode spanNode = doc.DocumentNode.Descendants("span")
-                    .FirstOrDefault(n => n.Attributes["class"]?.Value == spanClass);
+                // Select the span elements with the specified class name
+                //HtmlNodeCollection spanNodes = htmlDoc.DocumentNode.SelectNodes($"//span[contains(@class, '{className}')]");
+                HtmlNode spanNode = htmlDoc.DocumentNode.SelectSingleNode($"//span[@id='{spanId}']");
 
-                string spanValue = spanNode?.InnerText;
+                log.LogInformation($"is span nodes null? {htmlContent}");
 
-                if (!string.IsNullOrEmpty(spanValue))
+                if (spanNode != null)
                 {
-                    return new OkObjectResult(spanValue);
+                    // Extract the inner text of the span element
+                    string spanText = spanNode.InnerText;
+
+                    // Do something with the extracted text
+                    log.LogInformation($"Scraped span text: {spanText}");
                 }
                 else
                 {
-                    return new BadRequestObjectResult("Specified span ID not found in the HTML.");
+                    log.LogInformation("No span element found with the specified id.");
                 }
+
+                // Process the selected span elements
+                // if (spanNodes != null)
+                // {
+                //     foreach (HtmlNode spanNode in spanNodes)
+                //     {
+                //         // Extract the inner text of the span element
+                //         string spanText = spanNode.InnerText;
+
+                //         // Do something with the extracted text
+                //         log.LogInformation($"Scraped span text: {spanText}");
+                //     }
+                // }
+                // else
+                // {
+                //     log.LogInformation("No span elements found with the specified class name.");
+                // }
             }
-            catch
+            else
             {
-                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                log.LogError("Failed to retrieve the website content.");
             }
-        }
-        else
-        {
-            return new BadRequestObjectResult("Please provide 'url' and 'span_id' parameters.");
-        }
+            }
         }
     }
 }
